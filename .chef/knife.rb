2cc5d55810a9593b039bb136c1272505bcc5d8d7
revanth <revanthreddy36@gmai.com>
2016-08-01 16:41:07 -0700

    url update

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,5 +7,5 @@ node_name                "revanth"
 client_key               "#{current_dir}/revanth.pem"
 validation_name          "rr"
 validation_key           "#{current_dir}/rr.pem"
-chef_server_url          "https://54.187.84.171/organizations/rr"
+chef_server_url          "https://54.186.239.79/organizations/rr"
 cookbook_path            ["#{current_dir}/../cookbooks"]
